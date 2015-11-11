37ba36d708f53826d8c7073d7fd968179dc05a5e
Corey Edwards <corey@cedwardsmedia.com>
2015-11-10 21:21:37 -0600

    Fixed function isset issues
    Fixed an issue where PHP would not allow me to use isset() on the return of a function. Now, I set variables corresponding to the output of each function to get the details used by the card.

--- chipkard.php
+++ chipkard.php
@@ -1,13 +1,20 @@
 <?php
-   if (isset($site->twitter()) AND isset($page->twittercard()) AND isset($page->description())) {
- ?>
-<meta name="twitter:card" content="<?php $page->twittercard(); ?>" />
-<meta name="twitter:site" content="<?php $site->twitter(); ?>" />
-<meta name="twitter:title" content="<?php $page->title(); ?>" />
-<meta name="twitter:description" content="<?php $page->description(); ?>" />
-<meta name="twitter:image" content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg" />
+$twitter = $site->twitter();
+$cardtype = $page->twittercard();
+$description = $page->description();
+$image = $page->image('featured.jpg');
 
+   if (isset($twitter) AND isset($cardtype) AND isset($description)) {
+ ?>
+<meta name="twitter:card" content="<?php echo $page->twittercard(); ?>" />
+  <meta name="twitter:site" content="<?php echo $site->twitter(); ?>" />
+  <meta name="twitter:title" content="<?php echo $page->title(); ?>" />
+  <meta name="twitter:description" content="<?php echo $page->description(); ?>" />
 <?php
+   if ($image): ?>
+  <meta name="twitter:image" content="<?php echo $image->url() ?>" />
+<?php endif;
+
    } else {
       return null;
    }
