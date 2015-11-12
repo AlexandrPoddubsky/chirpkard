e5c184668f2b7580cd002555a9be1512e4e47732
Corey Edwards <corey@cedwardsmedia.com>
2015-11-11 23:56:34 -0600

    Added sanity check for card type

--- chipkard.php
+++ chipkard.php
@@ -5,6 +5,14 @@ $description = $page->description();
 $image = $page->image('featured.jpg');
 
    if (isset($twitter) AND isset($cardtype) AND isset($description)) {
+      if (!($cardtype == "summary" OR $cardtype == "summary_large_image" OR $cardtype == "photo" OR $cardtype == "gallery")) {
+         echo "<script>console.log(\"Invalid Twitter Card Type.\")</script>";
+         return null;
+      }
+      if ($cardtype == "summary_large_image" AND !isset($image)) {
+         echo "<script>console.log(\"Twitter Card Type is 'summary_large_image' but no featured image is available.\")</script>";
+         return null;
+      }
  ?>
 <meta name="twitter:card" content="<?php echo $page->twittercard(); ?>" />
   <meta name="twitter:site" content="<?php echo $site->twitter(); ?>" />
