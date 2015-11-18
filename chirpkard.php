ecb998be73b2fbaac61f56f236da0bec5ddf6412
Corey Edwards <corey@cedwardsmedia.com>
2015-11-17 21:38:53 -0500

    Added case for Gallery card type

--- chirpkard.php
+++ chirpkard.php
@@ -24,17 +24,32 @@ $image = $page->tcardimage();
 <?php endif;
 
    switch ($cardtype) {
-      case "summary": ?>
-      <meta name="twitter:app:country" content="<?php echo $page->country(); ?>">
-      <meta name="twitter:app:name:iphone" content="<?php echo $page->iphoneappname(); ?>">
-      <meta name="twitter:app:id:iphone" content="<?php echo $page->iphoneappid(); ?>">
-      <meta name="twitter:app:url:iphone" content="<?php echo $page->iphoneappurl(); ?>">
-      <meta name="twitter:app:name:ipad" content="<?php echo $page->ipadappname(); ?>">
-      <meta name="twitter:app:id:ipad" content="<?php echo $page->ipadappid(); ?>">
-      <meta name="twitter:app:url:ipad" content="ipadappurl">
-      <meta name="twitter:app:name:googleplay" content="<?php echo $page->googleappname(); ?>">
-      <meta name="twitter:app:id:googleplay" content="googleappid">
-      <meta name="twitter:app:url:googleplay" content="googleplayurl">
+      case "app": ?>
+         <meta name="twitter:app:country" content="<?php echo $page->country(); ?>">
+         <meta name="twitter:app:name:iphone" content="<?php echo $page->iphoneappname(); ?>">
+         <meta name="twitter:app:id:iphone" content="<?php echo $page->iphoneappid(); ?>">
+         <meta name="twitter:app:url:iphone" content="<?php echo $page->iphoneappurl(); ?>">
+         <meta name="twitter:app:name:ipad" content="<?php echo $page->ipadappname(); ?>">
+         <meta name="twitter:app:id:ipad" content="<?php echo $page->ipadappid(); ?>">
+         <meta name="twitter:app:url:ipad" content="ipadappurl">
+         <meta name="twitter:app:name:googleplay" content="<?php echo $page->googleappname(); ?>">
+         <meta name="twitter:app:id:googleplay" content="googleappid">
+         <meta name="twitter:app:url:googleplay" content="googleplayurl">
+<?php
+         break;
+      case "gallery": ?>
+         <meta name="twitter:url" content="<?php echo $page->url(); ?>" />
+
+<?php
+      $gallery_count = 0;
+      foreach($page->$images as $gallery_image):
+            ?>
+         <meta name="twitter:image<?php echo $gallery_count; ?>" content="<?php echo $gallery_image->url(); ?>">
+<?php
+      $gallery_count++;
+      endforeach
+?>
+
 <?php
    }
 
